4a685241e1cdb3eb8ca3f9ace31b00ace42c0786
May <maymchen1@gmail.com>
2023-08-12 18:20:05 -0400

    m

--- app/models/description.rb
+++ app/models/description.rb
@@ -1,2 +1,3 @@
 class Description < ApplicationRecord
+    belongs_to :game_posting
 end
--- app/models/game_posting.rb
+++ app/models/game_posting.rb
@@ -1,2 +1,5 @@
 class GamePosting < ApplicationRecord
+    has_many :player_game
+    has_many :player_profile, through: :player_game
+    has_one :description 
 end
