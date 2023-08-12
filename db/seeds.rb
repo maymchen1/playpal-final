768ee94b40f223c758d6b7838fe9be11ca6aaebb
May <maymchen1@gmail.com>
2023-08-12 19:01:56 -0400

    m

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# db/seeds.rb
+
+# Clear existing data
+PlayerGame.delete_all
+Description.delete_all
+GamePosting.delete_all
+Friends.delete_all
+PlayerProfile.delete_all
+
+# Create PlayerProfiles
+may = PlayerProfile.create(username: 'may', password: 'password1', email: 'may@mail.com')
+merty = PlayerProfile.create(username: 'merty', password: 'password2', email: 'merty@mail.com')
+kevin = PlayerProfile.create(username: 'kevin', password: 'password3', email: 'kevin@mail.com')
+crystal = PlayerProfile.create(username: 'crystal', password: 'password3', email: 'crystal@mail.com')
+
+# Create GamePostings with Descriptions
+valorant = GamePosting.create(title: 'Valorant')
+description1 = Description.create(genre: 'FPS', player_limit: 5, description_text: 'Valorant is a free-to-play first-person tactical hero shooter developed and published by Riot Games, for Windows. Teased under the codename Project A in October 2019, the game began a closed beta period with limited access on April 7, 2020, followed by a release on June 2, 2020.', 
+description_title: 'LF Radiant Smurfs')
+valorant.description = description1
+valorant.save
+
+baldur = GamePosting.create(title: 'Baldur Gate 3')
+description2 = Description.create(genre: 'Role Playing', player_limit: 4, description_text: 'Baldurs Gate 3 is a role-playing game that is developed and published by Larian Studios for Microsoft Windows with plans to release on MacOS, Playstatioon 5, & XBox Series X/S. Baldurs Gate 3 is the third installment in the Baldurs Gate series and it is set in the DnD universe from Wizards of the Coast.', 
+description_title: 'Need friends for chaotic run')
+baldur.description = description2
+baldur.save
+
+minecraft = GamePosting.create(title: 'Minecraft')
+description3 = Description.create(genre: 'Sandbox', player_limit: 30, description_text: 'Minecraft is a game where players place blocks and go on adventures. This includes anything from crafting simple items like containers or weapons, to building structures like houses, castles, and cities, or even making complex mechanical-devices', 
+description_title: 'Cozy Minecraft Run')
+minecraft.description = description3
+minecraft.save
+
+# Create Friends
+friendship1 = Friends.create(player_profile: may, friend: merty, invite_status: 'accepted')
+friendship2 = Friends.create(player_profile: may, friend: kevin, invite_status: 'pending')
+
+puts 'Seeding completed!'
