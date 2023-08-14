ac9c7a0de344fa9a7b5dc92d5087300d93ee2abf
May <maymchen1@gmail.com>
2023-08-13 20:30:35 -0400

    m

--- db/migrate/20230812215559_create_friends.rb
+++ db/migrate/20230812215559_create_friends.rb
@@ -3,6 +3,7 @@ class CreateFriends < ActiveRecord::Migration[7.0]
     create_table :friends do |t|
       t.string :invite_status
       t.references :player_profile, foreign_key: true
+      t.belongs_to :player_profile, null: false, foreign_key: true
 
 
       t.timestamps
--- db/migrate/20230812215823_create_descriptions.rb
+++ db/migrate/20230812215823_create_descriptions.rb
@@ -5,6 +5,7 @@ class CreateDescriptions < ActiveRecord::Migration[7.0]
       t.integer :player_limit
       t.string :description_title
       t.text :description_text
+      t.belongs_to :game_posting, null: false, foreign_key: true
 
       t.timestamps
     end
